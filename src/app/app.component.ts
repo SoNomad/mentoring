f0fa7eaf2c2942e4428ecc93f6fe63443dcd1e98
SoNomad <110348044+SoNomad@users.noreply.github.com>
2024-04-16 21:02:16 +0300

    Ссылка в хедера

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UsersListComponent } from './components/users-list/users-list.component';
-import { HeaderComponent } from './ui/header/header/header.component';
+import { HeaderComponent } from './ui/header/header.component';
 
 @Component({
   selector: 'app-root',
