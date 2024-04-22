dc1e35e562660fcbf6640622e561187a8718b3d2
SoNomad <110348044+SoNomad@users.noreply.github.com>
2024-04-22 23:46:19 +0300

    tap

--- src/app/store/users.effects.ts
+++ src/app/store/users.effects.ts
@@ -1,3 +1,22 @@
-import { createEffect } from '@ngrx/effects';
+import { Injectable, inject } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { UsersApiService } from '../services/users.api.service';
+import { UsersActions } from './users.actions';
+import { map, mergeMap } from 'rxjs';
 
-// export const loadUsers = createEffect();
+@Injectable()
+export class UsersEffects {
+  private readonly UsersApiService = inject(UsersApiService);
+  private readonly actions$ = inject(Actions);
+
+  getUsers$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(UsersActions.getUsers),
+      mergeMap(() => {
+        return this.UsersApiService.getUsers().pipe(
+          map((users) => UsersActions.getUsersSuccess({ users }))
+        );
+      })
+    )
+  );
+}
--- src/app/store/users.selectors.ts
+++ src/app/store/users.selectors.ts
@@ -1,3 +1,17 @@
+import { createSelector } from '@ngrx/store';
 import { AppStateType } from '../../types/UserStateType';
 
 export const selectFeature = (state: AppStateType) => state.users;
+
+export const selectLoading = createSelector(
+  selectFeature,
+  (state) => state.isLoading
+);
+export const selectUsers = createSelector(
+  selectFeature,
+  (state) => state.users
+);
+export const selectError = createSelector(
+  selectFeature,
+  (state) => state.errors
+);
